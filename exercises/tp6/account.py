fbe73833cb6263d902823f0f56925368753b3034
Agustin Alexander <agustin.c.alexander@gmail.com>
2023-09-12 09:11:48 -0300

    Update account.py

--- exercises/tp6/account.py
+++ exercises/tp6/account.py
@@ -1,28 +1,5 @@
 from __future__ import annotations
 
 
-class Fraction:
-
-    def __init__(self, n: int, d: int):
-        pass
-
-    def __repr__(self) -> str:
-        pass
-
-    def __eq__(self, other) -> bool:
-        pass
-
-    def __mul__(self, other: Fraction) -> Fraction:
-        pass
-
-    def __add__(self, other: Fraction) -> Fraction:
-        pass
-
-    def __sub__(self, other: Fraction) -> Fraction:
-        pass
-
-    def __truediv__(self, other: Fraction) -> Fraction:
-        pass
-
-def gcd(a: int, b: int) -> int:
+class Account:
     pass
