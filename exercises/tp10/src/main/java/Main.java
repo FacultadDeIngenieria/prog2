2fea92fdfb27fb3ffc95ea355f8b476d4af09906
Agustin Alexander <agustin.c.alexander@gmail.com>
2023-10-16 20:37:07 -0300

    Update Main.java

--- exercises/tp10/src/main/java/Main.java
+++ exercises/tp10/src/main/java/Main.java
@@ -1,1 +1,7 @@
 
+public class Main {
+    public static void main(String[] args) {
+        // TODO import account and run it, or make it run with tests
+        System.out.println("Bank Account System is running.");
+    }
+}
